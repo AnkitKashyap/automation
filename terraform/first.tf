df4649b887cca1afaddfbf1e0578f61721d66391
ankit <ankit.kashyap@payu.in>
2017-05-03 11:21:24 +0000

    Adding first terraform file to launch basic ec2 instance using default vpc

---
+++ terraform/first.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
+resource "aws_instance" "test1" {
+  ami           = "ami-b374d5a5"
+  instance_type = "t2.micro"
+}
