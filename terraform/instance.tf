8980e97ddc8b369b1f9b9a857e1046f668c41d58
ankit <ankit.kashyap@payu.in>
2017-05-04 17:54:47 +0000

    Launching instance in public subnet

---
+++ terraform/instance.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "web-1" {
+    ami = "${var.ami_id}"
+    availability_zone = "us-east-1a"
+    instance_type = "t1.micro"
+    key_name = "${aws_key_pair.terrakey.id}"
+    vpc_security_group_ids = ["${aws_security_group.web.id}"]
+    subnet_id = "${aws_subnet.us-east-public.id}"
+    source_dest_check = false
+
+
+    tags {
+        Name = "Web Server 1"
+    }
+}
