6cd074b449645a7c87edd9f075ade0a8743e3f29
ankit <ankit.kashyap@payu.in>
2017-05-04 17:07:42 +0000

    Adding security group

---
+++ terraform/securitygroup.tf
@@ -0,0 +1,37 @@
+resource "aws_security_group" "web" {
+    name = "vpc_web"
+    description = "Allow incoming HTTP connections."
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = -1
+        to_port = -1
+        protocol = "icmp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    vpc_id = "${aws_vpc.main.id}"
+
+    tags {
+        Name = "WebServerSG"
+    }
+}
+resource "aws_security_group" "db" {
+    name = "vpc_db"
+    description = "Allow incoming mysql connections."
+
+    ingress {
+        from_port = -1
+        to_port = -1
+        protocol = "icmp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    vpc_id = "${aws_vpc.main.id}"
+
+    tags {
+        Name = "DbServerSG"
+    }
+}
