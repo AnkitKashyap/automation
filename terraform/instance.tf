8f5b38c0b3f0683ff980adbba1df450d8b013cbd
ankit <ankit.kashyap@payu.in>
2017-05-04 18:12:14 +0000

    Add elastic IP

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web-1" {
     ami = "${var.ami_id}"
     availability_zone = "us-east-1a"
-    instance_type = "t1.micro"
+    instance_type = "t2.micro"
     key_name = "${aws_key_pair.terrakey.id}"
     vpc_security_group_ids = ["${aws_security_group.web.id}"]
     subnet_id = "${aws_subnet.us-east-public.id}"
